99228262ecaeae8b2b2ec7d7bdddb69903babb69
CalebBird00 <53249342+CalebBird00@users.noreply.github.com>
2020-04-20 16:29:39 -0400

    Made brute forcing process its own thread and made it multi-threaded

--- BruteForcer/BruteForcer/BruteForce.cs
+++ BruteForcer/BruteForcer/BruteForce.cs
@@ -1,10 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
 
 namespace BruteForcer
 {
     class BruteForce
     {
+
+        public static bool PasswordFound = false;
+        public static string Password = "ZZZ";
+
+        public static void Brute()
+        {
+            string[] Characters = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            while (PasswordFound == false)
+            {
+                string PasswordGuess;
+                int i = 0;
+                int B = 0;
+                while (i != 25 && PasswordFound == false)
+                {
+
+
+                    PasswordGuess = Characters[i];
+                    Trace.WriteLine(PasswordGuess);
+
+                    if (PasswordGuess == Password)
+                    {
+                        PasswordFound = true;
+                        Trace.WriteLine("Found the password!");
+                    }
+
+
+                    int A = 0;
+                    while (A < 25 && PasswordFound == false)
+                    {
+                        Trace.WriteLine(i.ToString() + " " + A.ToString());
+                        PasswordGuess = Characters[i] + Characters[A];
+                        Trace.WriteLine(PasswordGuess);
+
+                        if (PasswordGuess == Password)
+                        {
+                            PasswordFound = true;
+                            Trace.WriteLine("Found the password!");
+                        }
+                        A++;
+                    }
+                    i++;
+                }
+
+            }
+        }
     }
 }
--- BruteForcer/BruteForcer/MainWindow.xaml.cs
+++ BruteForcer/BruteForcer/MainWindow.xaml.cs
@@ -22,8 +22,8 @@ namespace BruteForcer
     /// </summary>
     public partial class MainWindow : Window
     {
-        public bool PasswordFound = false;
-        public string Password = "A";
+
+        public static int Threads = 10000;
         public MainWindow()
         {
             InitializeComponent();
@@ -38,55 +38,19 @@ namespace BruteForcer
         {
             Thread timer = new Thread(new ThreadStart(Timer));
             timer.Start();
-            PasswordFound = false;
-            BruteForce();
-        }
-
-        public void BruteForce(){
-            string[] Characters = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-            while (PasswordFound == false)
-            {
-                string PasswordGuess;
-                int i = 0;
-                int B = 0;
-                while (i != 25 && PasswordFound == false)
-                {
-
-
-                    PasswordGuess = Characters[i];
-                    Trace.WriteLine(PasswordGuess);
-                    
-                    if (PasswordGuess == Password)
-                    {
-                        PasswordFound = true;
-                        Trace.WriteLine("Found the password!");
-                    }
-
-
-                    int A = 0;
-                    while (A < 25 && PasswordFound == false)
-                    {
-                        Trace.WriteLine(i.ToString() + " " + A.ToString());
-                        PasswordGuess = Characters[i] + Characters[A];
-                        Trace.WriteLine(PasswordGuess);
-                       
-                        if (PasswordGuess == Password)
-                        {
-                            PasswordFound = true;
-                            Trace.WriteLine("Found the password!");
-                        }
-                        A++;
-                    }
-                    i++;
-                }
-        
+            for (int i = 0; i != Threads; i++){
+                Trace.WriteLine("Creating thread " + i.ToString());
+                Thread Brute = new Thread(new ThreadStart(BruteForce.Brute));
+                Brute.Start();
             }
+            BruteForce.PasswordFound = false;
+            BruteForce.Brute();
         }
 
         public void Timer()
         {
             double TimeTook = 0;
-            while (PasswordFound == false)
+            while (BruteForce.PasswordFound == false)
             {
                 Thread.Sleep(100);
                 TimeTook += 0.1;
